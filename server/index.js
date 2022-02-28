fdd9a9ed2ed75fe36f754af799cd17545f1f0a04
Kyle DeGuzman <kyle.deguzman.579@my.csun.edu>
2022-02-28 08:58:52 -0800

    new connection event listener

--- server/index.js
+++ server/index.js
@@ -2,9 +2,26 @@ const express = require("express");
 const app = express();
 const http = require('http'); // exist in all nodejs apps
 const cors = require('cors'); //library
+const {Server} = require("socket.io"); // imports class Server from socket io library
 
 app.use(cors());
 const server = http.createServer(app);
+const io = new Server(server, {
+    cors: {
+        origin: "http://localhost:3000",//tells socket io server which URL will be making requests
+        methods: ["GET", "POST"] //only accepts these requests
+
+    }
+});
+
+io.on("connection", (socket) => {//detecs if someone connects to socket server
+    console.log(socket.id); //when someone opens a website, they a unique id
+
+
+    socket.on("disconnect", () => { //when someone closes tab
+        console.log("User Disconnected", socket.id);
+    })
+})
 
 server.listen(3001, () => {
     console.log("SERVER RUNNING");
