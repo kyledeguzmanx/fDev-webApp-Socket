5e9321d14dd38b9e9ca61c01078e3e331ac6d1f7
Kyle DeGuzman <kyle.deguzman.579@my.csun.edu>
2022-03-17 23:57:12 -0700

    chat header

--- client/src/Components/Chat.jsx
+++ client/src/Components/Chat.jsx
@@ -25,7 +25,7 @@ function Chat({socket, username, room}){
     }, [socket]);
     return(
         <div className="chatroomActive">
-            <h1> Room ID {room.toLowerCase()}</h1>
+            <h1> Room ID: {room.toLowerCase()}</h1>
             <div className="chat-header">
                 <p> Live Chat</p>
             </div>
