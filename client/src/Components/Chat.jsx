0d8781f699986630dd541859ce378283e45f0271
Kyle DeGuzman <kyle.deguzman.579@my.csun.edu>
2022-03-19 23:55:42 -0700

    new id for join button

--- client/src/Components/Chat.jsx
+++ client/src/Components/Chat.jsx
@@ -48,7 +48,7 @@ function Chat({socket, username, room}){
             </div>
             <div className="chat-footer">
                 <input id="room-submit-btn" type="text" placeholder="Enter message" onChange={(event) => {setCurrentMessage(event.target.value)}}></input>
-                <button onClick={SendMessage} >&#9658;</button>
+                <button id="join-chatroom-btn"onClick={SendMessage} >&#9658;</button>
             </div>
         </div>
     )
